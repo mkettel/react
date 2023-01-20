1e7a98092b3196cd674f36a222886a4b50b9f032
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-01-20 13:29:23 -0500

    added a menuitem component that is dynamic to be able to add shit to the menu. Seems pretty solid to use

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,21 +6,45 @@ function App() {
   return (
     <div className="App">
       {name}
-      <Manager name="Ryan" age={25} email="ryan@mountainview.com"/>
+      <div className="header">
+        <p>Manager:</p><Manager name="Ryan" age={25} email="ryan@mountainview.com"/>
+      </div>
+      <div className="menu">
+        <div className="drinks">
+          <h3>Drinks</h3>
+          <MenuItem name="Dirty Chai" price={5.00}/>
+          <MenuItem name="Mocha" price={5.50}/>
+          <MenuItem name="Flat White" price={6.00}/>
+          <MenuItem name="Matcha Latte" price={4.00}/>
+          <MenuItem name="Cold Brew" price={3.00}/>
+        </div>
+        <div className="food">
 
+        </div>
+      </div>
     </div>
   );
 }
 
+
+// COMPONENTS ------------------------
 const Manager = (props) => {
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.email}</h1>
+    <div className='manager'>
+      <p>{props.name}</p>
+      <p>{props.age}</p>
+      <p>{props.email}</p>
     </div>
   )
 }
 
+const MenuItem = (props) => {
+  return (
+    <div className='menuItem'>
+       <p>{props.name}</p>
+       <p>${props.price}</p>
+    </div>
+  )
+}
 
 export default App;
