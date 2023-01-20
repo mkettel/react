b6ff0f218d3ba6df10d221295addb57fabea90b0
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-01-20 12:35:03 -0500

    made first react component that is called manager, that takes values: name, age, and email that is dynamic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = <h1 className='title'>Mountain View Coffee </h1>; // you can put the html as a variable then call it below which is very useful
   return (
     <div className="App">
-      <h1 className='title'>Howdy Eric, what is good?</h1>
+      {name}
+      <Manager name="Ryan" age={25} email="ryan@mountainview.com"/>
+
     </div>
   );
 }
+
+const Manager = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
+    </div>
+  )
+}
+
 
 export default App;
