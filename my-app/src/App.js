ea0a775452efa470d8e1e4ebd0332637b3172fc8
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-01-22 19:01:55 -0500

    added another way to make menu items based on iterating using the .map functionality. this should help out to organize data. Then if a new menu item is added, it can just be added to the array instead of a whole new HTML line

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 // this imports the css file to the application file
 import './App.css';
+import { useSyncExternalStore } from 'react';
 
 function App() {
   const name = <h1 className='title'>Mountain View Coffee </h1>; // you can put the html as a variable then call it below which is very useful
   const price = 15.00;
   const redText = false;
+  // drinks object below that could be used to get displayed in the menu or just to organize the data?
+  const drinks = [
+    { name: "Dirty Chai", price: 5.00 },
+    { name: "Mocha", price: 6.50 },
+    { name: "Flat White", price: 3.50 },
+    { name: "Matcha Latte", price: 5.50 },
+  ];
   return (
     <div className="App">
       {name}
@@ -25,9 +33,14 @@ function App() {
           <MenuItem name="Scone" price={2.75}/>
           <MenuItem name="Muffin Assortment" price={3.00}/>
         </div>
+      </div>
+      <div className="menu-two">
+        {drinks.map((drink, key) => {
+          return <Drink name={drink.name} price={drink.price} />
+        })}
+      </div>
       <div className="management">
         <p>Manager:</p><Manager name="Ryan" age={25} email="ryan@mountainview.com"/>
-      </div>
       </div>
     </div>
   );
@@ -53,5 +66,15 @@ const MenuItem = (props) => {
     </div>
   )
 }
+
+const Drink = (props) => {
+  return (
+    <div>
+      {props.name} {props.price}
+    </div>
+  )
+}
+
+
 
 export default App;
