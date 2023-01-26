48aa0661d028007b9cdd7fd6a789ae81f6df7cdb
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-01-25 21:28:45 -0600

    hid a lot of my code so that i can play with one large data set made up of a menu object that has data values defining its attributes then i can .map through the object to display only the information that i want

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,10 +16,35 @@ function App() {
     { name: "Matcha Vodka Latte", price: 5.50 },
     { name: "Cold ASF Brew", price: 3.00 }
   ];
+  // detailed menu to loop through
+  const detailedMenu = [
+    { name: "Drip Coffee", price: 2.50, isFood: false },
+    { name: "Cappucino", price: 3.50, isFood: false },
+    { name: "Mocha", price: 3.50, isFood: false },
+    { name: "Frappe", price: 5.50, isFood: false },
+    { name: "Donut", price: 2.00, isFood: true },
+    { name: "Chicken Strips", price: 5.50, isFood: true },
+    { name: "Sausage, Egg & Cheese Sandwich", price: 6.00, isFood: true },
+    { name: "Sausage, Bacon & Cheese Sandwich", price: 6.00, isFood: true },
+    { name: "Cesar Salad", price: 7.00, isFood: true },
+    { name: "Iced Tea", price: 3.00, isFood: false },
+    { name: "Espresso Shot", price: 6.00, isFood: false }
+  ]
   return (
     <div className="App">
+       {/* // navbar */}
+      <div className='nav-container'>
+        <div className='nav-left'>
+        </div>
+        <div className='nav-right'>
+          <p>Menu</p>
+          <p>Contact</p>
+          <p>Location</p>
+        </div>
+      </div>
       {name}
-      {price >= 15.00 ? <p>Add a free cookie?</p> : <p>Spend $15 or more to get a free cookie!</p>}
+      {/* {price >= 15.00 ? <p>Add a free cookie?</p> : <p>Spend $15 or more to get a free cookie!</p>} */}
+      {/* hiding all of this for now */}
       <div className="menu">
         <div className="drinks">
           <h3 style={{color: redText ? "red" : "black"}}>Drinks</h3>
@@ -37,12 +62,26 @@ function App() {
         </div>
       </div>
       <div className="drinks">
+        <h3>Secret Menu</h3>
         {secretDrinks.map((drink, key) => {
           return <SecretDrink name={drink.name} price={drink.price} />
         })}
       </div>
       <div className="management">
         <p>Manager:</p><Manager name="Ryan" age={25} email="ryan@mountainview.com"/>
+      </div>
+      {/* <hr /> */}
+
+      <div className="main-menu">
+        <div className='menu-two'>
+          <h3>Food Menu</h3>
+          {detailedMenu.map((item, key) => item.isFood && <p> {item.name} </p>
+          )}
+        </div>
+        <div className="menu-two">
+          <h3>Drink Menu</h3>
+          {detailedMenu.map((item, key) => !item.isFood && <p> {item.name} </p> )}
+        </div>
       </div>
     </div>
   );
